33a4f00c73469176fc0e74178f63929462994ba2
Dicky_asqaelani26 <93518546+Dickybulin26@users.noreply.github.com>
2025-10-22 22:49:57 +0700

    feature: Double Check Box Condition

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,6 +1,17 @@
 ﻿Imports System.Reflection.Emit
 
 Public Class Form1
+    Private Sub UpdateFontStyle()
+        Dim style As FontStyle = FontStyle.Regular
+        If CheckBox1.Checked Then
+            style = style Or FontStyle.Italic
+        End If
+        If CheckBox2.Checked Then
+            style = style Or FontStyle.Bold
+        End If
+        Label2.Font = New Font(Label2.Font, style)
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label2.Text = TextBox1.Text
     End Sub
@@ -15,14 +26,7 @@ Public Class Form1
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-        If CheckBox1.Checked Then
-            Label2.Font = New Font(Label2.Font, FontStyle.Italic)
-        ElseIf CheckBox1.Checked And CheckBox2.Checked Then
-            Label2.Font = New Font(Label2.Font, FontStyle.Italic And FontStyle.Bold)
-        Else
-            Label2.Font = New Font(Label2.Font, FontStyle.Regular)
-
-        End If
+        UpdateFontStyle()
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
